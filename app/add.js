db05b88649db0615b9c488081609d3223404f49a
Thiago <contact.thiago.ferreira@gmail.com>
2023-12-21 10:56:12 +0100

    init node

---
+++ app/add.js
@@ -0,0 +1,9 @@
+export const addUpTo = (n) =>{
+    
+    let total = 0
+    for (let i = 0; i < n; i++) {
+        total += i+1
+    
+    }
+    return total
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+import { addUpTo } from "./app/add.js";
+
+console.log(addUpTo(6))
